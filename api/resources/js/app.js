858935aa4021912c9cd07a52b37ca9ee4b0a4f32
Johan De Klerk <johandk996@gmail.com>
2019-09-13 18:25:23 +0200

    add laravel/ui with jquery/bootstrap

--- api/resources/js/app.js
+++ api/resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+
+$("#submit").click(function() {
+	alert('hello');
+});
--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -1,1 +1,19 @@
-<h1>PriceCheck Checker API</h1>
+<html>
+
+<head>
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+
+	<h1>PriceCheck Checker API</h1>
+
+	<textarea id="json-str" name="input"></textarea>
+
+	<button id="submit">Submit</button>
+
+	<script src="{{ asset('js/app.js') }}" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
